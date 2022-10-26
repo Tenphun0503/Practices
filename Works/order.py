7d80903ec96fedf08eec7d5de9f23f75cce739b8
Tenphun0503_PC <tenzinphuntsok0503@gmail.com>
2022-10-25 22:55:30 -0400

    added a small project

---
+++ Works/order.py
@@ -0,0 +1,50 @@
+import pizzaReceipt as pr
+
+SIZE = ['S', 'M', 'L', 'XL']
+NO = ['NO', 'Q', 'N']
+YES = ['YES', 'Y']
+TOPLIST = []
+
+
+def order_pizza():
+    while True:
+        size = input("Choose size").upper()
+        if size not in SIZE:
+            print("wrong size input, try again")
+        else:
+            break
+
+    toppings = []
+    while True:
+        topping = input("LIST to show topping\tX to stop adding").upper()
+        if topping == 'X':
+            break
+        elif topping == 'LIST':
+            print(TOPLIST)
+        elif topping not in TOPLIST:
+            print("we don't have " + topping + ", try again")
+        else:
+            toppings.append(topping)
+
+    return size, toppings
+
+
+if __name__ == "__main__":
+    aPizza = input("Do u want?").upper()
+    pizza = ''
+    if aPizza in NO:
+        print('Thank you')
+    elif aPizza in YES:
+        pizza = [order_pizza()]
+        while True:
+            continueOrder = input("do u wanna more?")
+            if continueOrder in NO:
+                break
+            elif continueOrder in YES:
+                pizza.append(order_pizza())
+            else:
+                print("wrong input")
+
+    pr.generateReceipt(pizza)
+
+
---
+++ Works/pizzaReceipt.py
@@ -0,0 +1,6 @@
+def generateReceipt(pizzaOrder):
+    if pizzaOrder == '':
+        print('You did not order anything')
+
+a = 'Zzzz'
+print(a.upper())
